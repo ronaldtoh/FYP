9382a03b02ae0d3be812e83e6077a0b5fe1fb0bb
Ronald Toh <jyronaldtoh@gmail.com>
2022-12-16 15:34:45 +0800

    Refactor code

--- multiplatemodel_THZ.py
+++ multiplatemodel_THZ.py
@@ -21,79 +21,10 @@ class MultiPlateModel_THZ:
     def __init__(self, N: int, Cab, Cw, Cm):
         "Leaf Structure Parameter"
         self.N = N
-        self.frequency = np.linspace(1.6, 0.2, 100)
-        self.wavelength = 3e8 / (self.frequency * 10e12) * 1000
-        # print(self.wavelength)
-        self.refractive_index_array = np.linspace(2.1, 1.7, 100)
-        self.dry_matter_absorption_coeff = np.linspace(5, 140, 100)
-        self.water_absorption_coeff = np.linspace(100, 300, 100)
-        self.albino_absorption_coeff = np.linspace(50, 200, 100)
-        # self.scatter = abs_scatter(self.frequency, 40 ,)
-        # This k right now is the absorption coefficient.
-        # It is derived from adding:
-        # [(Chlorophyll (a+b) content of leaf) * (Specific absorption coefficient of leaf) +
-        # (Water content of leaf) * (Specific absorption coefficient of leaf) +
-        # (Dry Matter content of leaf) * (Specific absorption coefficient of leaf)] / Leaf Structure Parameter
-        # + Absorption coefficient of albino elementary layer
-        # + Scattering-induced absorption
-        a_k = (
-            Cw * self.water_absorption_coeff  # + Cm * self.dry_matter_absorption_coeff
-        ) / N
-
-        # print(get_tav_THZ(90, self.refractive_index_array, k, self.frequency))
-        # Getting the transmission coefficient k
-        # Should never hit the if statement
-        if np.all(a_k <= 0):
-            k = 1
-        else:
-            k = (1 - a_k) * np.exp(-a_k) + (a_k**2) * exp1(a_k)
-        t1 = get_tav_THZ(90, self.refractive_index_array, a_k, self.wavelength)
-        t2 = get_tav_THZ(40, self.refractive_index_array, a_k, self.wavelength)
-        print(t2)
-        print(t1)
-        x1 = 1 - t1
-        x2 = (t1) ** 2 * (k**2) * (self.refractive_index_array**2 - t1)  # * t2
-        x3 = (t1) ** 2 * k * (self.refractive_index_array**2)  # * t2
-        x4 = (self.refractive_index_array**4) - (k**2) * (
-            (self.refractive_index_array**2 - t1) ** 2
-        )
-        x5 = t2 / t1
-        x6 = x5 * (t1 - 1) + 1 - t2
-
-        "r_90/t_90: Reflectance/Transmitance for remaining plates since light is isotropic in leaf"
-        self.r_90 = x1 + x2 / x4
-        self.t_90 = x3 / x4
-        "r_alpha/t_aplha: Reflectance/Transmitance for first plate"
-        self.r_alpha = x5 * self.r_90 + x6
-        self.t_alpha = x5 * self.t_90
-        # print(f"r_90: {self.r_90}")
-        # print(f"t_90: {self.t_90}")
-        # print(f"r_alpha: {self.r_alpha}")
-        # print(f"t_alpha: {self.t_alpha}")
-
-        self.delta = self._delta()
-        self.alpha = self._alpha()
-        self.beta = self._beta()
-
-        # print(f"beta: {self.beta}")
-        # print(f"delta: {self.delta}")
-        # print(f"alpha: {self.alpha}")
-        self.b = self._b()
-
-        self.s1 = self.r_alpha * (
-            self.alpha * self.b ** (self.N - 1)
-            - (self.alpha**-1) * (self.b ** (1 - self.N))
-        ) + (self.t_alpha * self.t_90 - self.r_alpha * self.r_90) * (
-            self.b ** (self.N - 1) - self.b ** (1 - self.N)
-        )
-
-        self.s2 = self.t_alpha * (self.alpha - self.alpha**-1)
-
-        self.s3 = (
-            self.alpha * (self.b ** (self.N - 1))
-            - (self.alpha**-1) * (self.b ** (1 - self.N))
-            - self.r_90 * (self.b ** (self.N - 1) - self.b ** (1 - self.N))
-        )
+        self.Cab = Cab
+        self.Cw = Cw
+        self.Cm = Cm
+        self.calculate()
 
     def _b(self):
         return (
@@ -114,52 +45,112 @@ class MultiPlateModel_THZ:
     def _delta(self):
         return (self.t_90**2 - self.r_90**2 - 1) ** 2 - (4 * self.r_90**2)
 
-    def reflectance(self):
+    def _reflectance(self):
         return self.s1 / self.s3
 
-    def transmittance(self):
+    def _transmittance(self):
         return self.s2 / self.s3
 
+    def calculate(self):
+        self.frequency = np.linspace(0.2, 1.6, 100)
+        self.wavelength = 3e8 / (self.frequency * 10e12) * 1000
+
+        self.refractive_index_array = np.linspace(2.1, 1.7, 100)
+        self.dry_matter_absorption_coeff = np.linspace(5, 140, 100)
+        self.water_absorption_coeff = np.linspace(100, 300, 100)
+        self.albino_absorption_coeff = np.linspace(50, 200, 100)
+        # self.scatter = abs_scatter(self.frequency, 40 ,)
+        # This a_k is the absorption coefficient.
+        # It is derived from adding:
+        # [(Chlorophyll (a+b) content of leaf) * (Specific absorption coefficient of leaf) +
+        # (Water content of leaf) * (Specific absorption coefficient of leaf) +
+        # (Dry Matter content of leaf) * (Specific absorption coefficient of leaf)] / Leaf Structure Parameter
+        # + Absorption coefficient of albino elementary layer
+        # + Scattering-induced absorption
+        a_k = (
+            self.Cw * self.water_absorption_coeff
+            + self.Cm * self.dry_matter_absorption_coeff
+        ) / self.N
+
+        # Getting the transmission coefficient k
+        # Should never hit the if statement
+        if np.all(a_k <= 0):
+            k = 1
+        else:
+            k = (1 - a_k) * np.exp(-a_k) + (a_k**2) * exp1(a_k)
+        t1 = get_tav_THZ(90, self.refractive_index_array, a_k, self.wavelength)
+        t2 = get_tav_THZ(40, self.refractive_index_array, a_k, self.wavelength)
+
+        x1 = 1 - t1
+        x2 = (t1) ** 2 * (k**2) * (self.refractive_index_array**2 - t1)  # * t2
+        x3 = (t1) ** 2 * k * (self.refractive_index_array**2)  # * t2
+        x4 = (self.refractive_index_array**4) - (k**2) * (
+            (self.refractive_index_array**2 - t1) ** 2
+        )
+        x5 = t2 / t1
+        x6 = x5 * (t1 - 1) + 1 - t2
+
+        "r_90/t_90: Reflectance/Transmitance for remaining plates since light is isotropic in leaf"
+        self.r_90 = x1 + x2 / x4
+        self.t_90 = x3 / x4
+        "r_alpha/t_aplha: Reflectance/Transmitance for first plate"
+        self.r_alpha = x5 * self.r_90 + x6
+        self.t_alpha = x5 * self.t_90
+
+        self.delta = self._delta()
+        self.alpha = self._alpha()
+        self.beta = self._beta()
+
+        self.b = self._b()
+
+        self.s1 = self.r_alpha * (
+            self.alpha * self.b ** (self.N - 1)
+            - (self.alpha**-1) * (self.b ** (1 - self.N))
+        ) + (self.t_alpha * self.t_90 - self.r_alpha * self.r_90) * (
+            self.b ** (self.N - 1) - self.b ** (1 - self.N)
+        )
+
+        self.s2 = self.t_alpha * (self.alpha - self.alpha**-1)
+
+        self.s3 = (
+            self.alpha * (self.b ** (self.N - 1))
+            - (self.alpha**-1) * (self.b ** (1 - self.N))
+            - self.r_90 * (self.b ** (self.N - 1) - self.b ** (1 - self.N))
+        )
+
     def output(self):
-        return [self.frequency, self.reflectance(), self.transmittance()]
+        return [self.frequency, self._reflectance(), self._transmittance()]
+
+    def summary(self, title="test"):
+        plot, axs = plt.subplots()
+        output = self.output()
+        axs.plot(output[0], output[1], label="Reflectance")
+        axs.plot(output[0], output[2], label="Transmittance")
+
+        total = output[1] + output[2]
+        absorbed = 1 - total
+        axs.plot(output[0], absorbed, label="Absorbed")
+
+        axs.set_title(
+            f"Reflectance, Transmittance,Absorbance of light through a {title} leaf"
+        )
+        axs.set_xlabel("frequency/Thz")
+        axs.set_ylabel("")
+        axs.set_ylim(-0.2, 1.2)
+
+        axs.grid(which="both", color=(0.8, 0.8, 0.8))
+        plt.legend(loc=(1.04, 0.5))
+        plt.legend(bbox_to_anchor=(1, 0.4), loc="center right")
+        path = "output " + title + ".jpg"
+        plot.savefig(path)
 
 
 if __name__ == "__main__":
     # model = MultiPlateModel_THZ(1.518, 58, 0.00131, 0.003662)
-    model = MultiPlateModel_THZ(1.2, 30, 0.0015, 0.01)
+    # model = MultiPlateModel_THZ(1.2, 30, 0.0015, 0.01)
     # model = MultiPlateModel_THZ(2.2, 30, 0.0015, 0.0005)
     # model = PROSPECT1990(2.698, 70.8, 0.000117, 0.009327)
-    # df = pd.DataFrame(model.output())
-    output = model.output()
-    # print(f"reflectance: {output[1]}")
-    # print(f"transmittance: {output[2]}")
-
-    plot, axs = plt.subplots()
-    axs.plot(output[0], output[1], label="Reflectance")
-    axs.plot(output[0], output[2], label="Transmittance")
-
-    total = output[1] + output[2]
-    absorbed = 1 - total
-    axs.plot(output[0], absorbed, label="Absorbed")
-    # axs.hlines(y=1, xmin=400, xmax=2500, color="k")
-    # axs.plot(output[0], total, label="R+T")
-    axs.set_title("Reflectance, Transmittance of light through a leaf")
-    axs.set_xlabel("frequency/Thz")
-    axs.set_ylabel("")
-    # Set axis ranges; by default this will put major ticks every 25.
-    # axs.set_xlim(400, 2500)
-    axs.set_ylim(-0.2, 1.2)
-
-    # # Change major ticks to show every 20.
-    # axs.xaxis.set_major_locator(MultipleLocator(500))
-    # axs.yaxis.set_major_locator(MultipleLocator(0.2))
-
-    # # Change minor ticks to show every 5. (20/4 = 5)
-    # axs.xaxis.set_minor_locator(AutoMinorLocator(5))
-    # axs.yaxis.set_minor_locator(AutoMinorLocator(4))
-
-    axs.grid(which="both", color=(0.8, 0.8, 0.8))
-    plt.legend(loc=(1.04, 0.5))
-    plt.legend(bbox_to_anchor=(1, 0.4), loc="center right")
-
-    plot.savefig("test_THZ.jpg")
+    # output = model.output()
+    # model = MultiPlateModel_THZ(2.107, 35.2, 0.000244, 0.00225)  # Dry Lettuce
+    model = MultiPlateModel_THZ(2.275, 23.7, 0.0075, 0.005811)  # Fresh Rice
+    model.summary("fresh rice")
--- plot_data.py
+++ plot_data.py
@@ -1,7 +1,7 @@
 from data_another_style import data
 import numpy as np
 import matplotlib.pyplot as plt
-
+from multiplatemodel_THZ import MultiPlateModel_THZ
 
 database = np.array(data)
 
@@ -68,10 +68,102 @@ class PlotData:
         plt.savefig("comparison_dry_matter.jpg")
         return
 
+    def plot_varying_cw(self):
+        cw_values = [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.02]
+        fig, (ax1, ax2, ax3) = plt.subplots(
+            1, 3, figsize=(12, 5), sharey=True, sharex=True
+        )
+
+        fig.suptitle("Varying Cw")
+        ax1.set_title("Reflectance")
+        ax1.set_ylim(-0.2, 1.2)
+        ax1.grid()
+        ax1.set_ylabel("Coefficient")
+
+        ax2.set_title("Transmittance")
+        ax2.set_ylim(-0.2, 1.2)
+        ax2.grid()
+        ax2.set_xlabel("Frequency (THz)")
+
+        ax3.set_title("Absorbance")
+        ax3.set_ylim(-0.2, 1.2)
+        ax3.grid()
+        for i in cw_values:
+            model = MultiPlateModel_THZ(1.2, 30, i, 0.01)
+            output = model.output()
+            absorbance = 1 - output[1] - output[2]
+            ax1.plot(output[0], output[1], label=i)
+            ax2.plot(output[0], output[2], label=i)
+            ax3.plot(output[0], absorbance, label=i)
+        ax3.legend(loc="right", bbox_to_anchor=(1.4, 0.5))
+        fig.savefig("vary_cw.jpg")
+
+    def plot_varying_cm(self):
+        cw_values = [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.02]
+        fig, (ax1, ax2, ax3) = plt.subplots(
+            1, 3, figsize=(12, 5), sharey=True, sharex=True
+        )
+
+        fig.suptitle("Varying Cm")
+        ax1.set_title("Reflectance")
+        ax1.set_ylim(-0.2, 1.2)
+        ax1.grid()
+        ax1.set_ylabel("Coefficient")
+
+        ax2.set_title("Transmittance")
+        ax2.set_ylim(-0.2, 1.2)
+        ax2.grid()
+        ax2.set_xlabel("Frequency (THz)")
+
+        ax3.set_title("Absorbance")
+        ax3.set_ylim(-0.2, 1.2)
+        ax3.grid()
+        for i in cw_values:
+            model = MultiPlateModel_THZ(1.2, 30, 0.001, i)
+            output = model.output()
+            absorbance = 1 - output[1] - output[2]
+            ax1.plot(output[0], output[1], label=i)
+            ax2.plot(output[0], output[2], label=i)
+            ax3.plot(output[0], absorbance, label=i)
+        ax3.legend(loc="right", bbox_to_anchor=(1.4, 0.5))
+        fig.savefig("vary_cm.jpg")
+
+    def plot_varying_N(self):
+        N_values = [1, 1.5, 2, 2.5, 3]
+        fig, (ax1, ax2, ax3) = plt.subplots(
+            1, 3, figsize=(12, 5), sharey=True, sharex=True
+        )
+
+        fig.suptitle("Varying N")
+        ax1.set_title("Reflectance")
+        ax1.set_ylim(-0.2, 1.2)
+        ax1.grid()
+        ax1.set_ylabel("Coefficient")
+
+        ax2.set_title("Transmittance")
+        ax2.set_ylim(-0.2, 1.2)
+        ax2.grid()
+        ax2.set_xlabel("Frequency (THz)")
+
+        ax3.set_title("Absorbance")
+        ax3.set_ylim(-0.2, 1.2)
+        # ax3.set_xlim(0.2, 0.4)
+        ax3.grid()
+        for i in N_values:
+            model = MultiPlateModel_THZ(i, 30, 0.001, 0.01)
+            output = model.output()
+            absorbance = 1 - output[1] - output[2]
+            ax1.plot(output[0], output[1], label=i)
+            ax2.plot(output[0], output[2], label=i)
+            ax3.plot(output[0], absorbance, label=i)
+        ax3.legend(loc="right", bbox_to_anchor=(1.4, 0.5))
+        fig.savefig("vary_N.jpg")
+
 
 if __name__ == "__main__":
     plotter = PlotData()
-    plotter.plot_water_comparison()
-    plotter.plot_dry_matter_comparison()
+    # plotter.plot_water_comparison()
+    # plotter.plot_dry_matter_comparison()
+    plotter.plot_varying_N()
 
     pass
